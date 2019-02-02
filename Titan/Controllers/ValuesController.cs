2366bae2059e63cb3422608288735dd11704292d
Kasunjith-Bimal <kasunysoft@gmail.com>
2019-02-03 01:31:12 +0530

    add log file and swagger configuration

--- Titan/Controllers/ValuesController.cs
+++ Titan/Controllers/ValuesController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Titan.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        readonly ILogger<ValuesController> _log;
+
+        public ValuesController(ILogger<ValuesController> log)
+        {
+            _log = log;
+        }
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            _log.LogInformation("API Work");
             return new string[] { "value1", "value2" };
+            
         }
 
         // GET api/values/5
--- Titan/Startup.cs
+++ Titan/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
+using Swashbuckle.AspNetCore.Swagger;
 namespace Titan
 {
     public class Startup
@@ -24,17 +24,39 @@ namespace Titan
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            loggerFactory.AddFile("Logs/Titan-{Date}.txt");
 
             app.UseMvc();
+
+            app.UseStaticFiles();
+
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+           
         }
     }
 }
